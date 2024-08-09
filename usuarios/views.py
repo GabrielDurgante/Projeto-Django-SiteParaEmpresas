bcc73862ad7f980e9882bd12a6c1bbdb77a828c0
Gabriel <gabrielf.durgante@gmail.com>
2024-08-09 00:06:20 -0300

    cadastro com html mais bonito, e agora funcional

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
-def cadastro(request): 
-    return render(request, 'cadastro.html')
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            print(1)
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            print(2)
+            return redirect('/usuarios/cadastro')
+        
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            print(3)
+            return redirect('/usuarios/cadastro')
+
+
+        user = User.objects.create_user(
+            username=username,
+            password=senha
+        )
+
+        return redirect('/usuarios/logar')
