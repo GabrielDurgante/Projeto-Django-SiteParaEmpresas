bf8ba4efd482205fe73ba175151dec10cf0d512e
Gabriel <gabrielf.durgante@gmail.com>
2024-08-12 00:42:56 -0300

    Adcionando a função para ver empresa e adcionar/excluir documentos

--- empresarios/admin.py
+++ empresarios/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Empresas
+from .models import Empresas, Documento
 # Register your models here.
 
 admin.site.register(Empresas)
+admin.site.register(Documento)
 
 
 
--- empresarios/views.py
+++ empresarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Empresas
+from .models import Empresas, Documento
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -62,3 +62,56 @@ def listar_empresas(request):
         empresas = Empresas.objects.filter(user=request.user)
         return render(request, 'listar_empresas.html', {'empresas': empresas})
     
+
+def empresa(request, id):
+    empresa = Empresas.objects.get(id=id)
+
+    if empresa.user != request.user:
+        messages.add_message(request, constants.ERROR, "Essa empresa não é sua")
+        return redirect(f'/empresarios/listar_empresas/')
+
+    if request.method == "GET":
+        documentos = Documento.objects.filter(empresa=empresa)
+        return render(request, 'empresa.html', {'empresa': empresa, 'documentos': documentos})
+    
+    
+def add_doc(request, id):
+    empresa = Empresas.objects.get(id=id)
+    titulo = request.POST.get('titulo')
+    arquivo = request.FILES.get('arquivo')
+    extensao = arquivo.name.split('.')
+
+    if empresa.user != request.user:
+        messages.add_message(request, constants.ERROR, "Essa empresa não é sua")
+        return redirect(f'/empresarios/listar_empresas/')
+
+    if extensao[1] != 'pdf':
+        messages.add_message(request, constants.ERROR, "Envie apenas PDF's")
+        return redirect(f'/empresarios/empresa/{empresa.id}')
+    
+    if not arquivo:
+        messages.add_message(request, constants.ERROR, "Envie um arquivo")
+        return redirect(f'/empresarios/empresa/{empresa.id}')
+        
+    documento = Documento(
+        empresa=empresa,
+        titulo=titulo,
+        arquivo=arquivo
+    )
+    documento.save()
+    messages.add_message(request, constants.SUCCESS, "Arquivo cadastrado com sucesso")
+    return redirect(f'/empresarios/empresa/{empresa.id}')
+
+
+def excluir_dc(request, id):
+    documento = Documento.objects.get(id=id)
+    documento.delete()
+
+
+    if documento.empresa.user != request.user:
+        messages.add_message(request, constants.ERROR, "Esse documento não é seu")
+        return redirect(f'/empresarios/empresa/{documento.empresa.id}')
+
+
+    messages.add_message(request, constants.SUCCESS, "Documento excluído com sucesso")
+    return redirect(f'/empresarios/empresa/{documento.empresa.id}')
